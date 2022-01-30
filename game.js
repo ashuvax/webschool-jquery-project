90d7f8b9531e698edb93ddeead7fa998095ccc3f
ashuvax <ash6783397@gmail.com>
2022-01-30 18:16:48 +0200

    f

---
+++ game.js
@@ -0,0 +1,11 @@
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
+const colors = ["yellow", "green", "red", "blue"];
+function init() {
+  gamePattern = [];
+  userClickedPattern = [];
+  started = false;
+  level = 0;
+}
